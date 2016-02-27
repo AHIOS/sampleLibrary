cde405680cbbbf67dd3a4d3ed62afa0660c2f9d4
Giuseppe Valenti <g.valenti@wepush.org>
2016-02-27 14:24:22 +0100

    1st working exampleswith YT video

---
+++ Pod/Classes/MSLWebViewController.h
@@ -0,0 +1,13 @@
+//
+//  MSLWebViewController.h
+//  Pods
+//
+//  Created by Giuseppe Valenti on 2/27/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSLWebViewController : UIViewController
+
+@end
